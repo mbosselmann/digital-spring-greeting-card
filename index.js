c87812ee2579fd8aa165e929aa0a9931ac76a2ac
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2024-04-02 09:37:46 +0200

    update card

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const card = document.querySelector(".card");
+const flower = document.querySelector(".flower");
+
 card.addEventListener("click", () => {
   card.classList.toggle("flipcard");
+  flower.classList.toggle("rotate");
 });
