879c78eb4020af6b9d8b0dea1355ea8d9bbbe75c
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2024-04-16 14:16:33 +0200

    update starter

--- .where-the-magic-is/index.js
+++ .where-the-magic-is/index.js
@@ -3,5 +3,8 @@ const flower = document.querySelector(".flower");
 
 card.addEventListener("click", () => {
   card.classList.toggle("flipcard");
-  flower.classList.toggle("rotate");
+
+  if (flower) {
+    flower.classList.toggle("rotate");
+  }
 });
